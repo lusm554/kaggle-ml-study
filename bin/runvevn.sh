b24e3c119d7d86539f59cb5c6b676bd7498fbe79
lusm <loveyousomuch554@gmail.com>
2023-07-11 16:11:13 +0300

    support packages from requrementstxt

--- bin/runvevn.sh
+++ bin/runvevn.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 python3 -m venv .venv --system-site-packages # create virtual environment
 source .venv/bin/activate # activate the venv
+if test -f "requirements.txt"; then
+  pip3 install -r requirements.txt
+fi
 python -m ipykernel install --user --name=.venv # register env in jupyter lab, it'll show up in kernels list
